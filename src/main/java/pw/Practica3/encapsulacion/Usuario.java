c8e6e98fbd0e7fe94a7bdbde406f190cbd6de182
jmlemoine <jeanmelvinlp27@gmail.com>
2019-10-22 19:46:38 -0400

    Creando las variables, constructor, get y set de la clase Usuario

--- src/main/java/pw/Practica3/encapsulacion/Usuario.java
+++ src/main/java/pw/Practica3/encapsulacion/Usuario.java
@@ -1,4 +1,69 @@
 package pw.Practica3.encapsulacion;
 
 public class Usuario {
+    private long id;
+    private String username;
+    private String password;
+    private boolean administrador;
+    private boolean autor;
+
+    public Usuario(long id, String username, String password, boolean administrador, boolean autor) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.administrador = administrador;
+        this.autor = autor;
+    }
+
+    public Usuario() {
+    }
+
+    @Override
+    public String toString() {
+        String mensaje="El username del usuario es: "+username;
+        return mensaje;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdministrador() {
+        return administrador;
+    }
+
+    public void setAdministrador(boolean administrador) {
+        this.administrador = administrador;
+    }
+
+    public boolean isAutor() {
+        return autor;
+    }
+
+    public void setAutor(boolean autor) {
+        this.autor = autor;
+    }
+
+
 }
